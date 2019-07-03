44378901e2dbee0e155c3c2ac559c05f0caf7f67
CarolineChapeau <caroline2chapeau@gmail.com>
2019-07-03 14:00:02 +0200

    adding function to create an employee

--- src/Controller/SuperAdmin/EmployeeController.php
+++ src/Controller/SuperAdmin/EmployeeController.php
@@ -3,14 +3,20 @@
 
 namespace App\Controller\SuperAdmin;
 
+use App\Form\UserType;
 use App\Repository\UserRepository;
+use App\User\UserRequest;
+use App\User\UserRequestHandler;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 /**
  * Class EmployeeController
  * @package App\Controller\SuperAdmin
- * @Route("/employe")
+ * @Route("/employes")
  */
 class EmployeeController extends AbstractController
 {
@@ -23,6 +29,41 @@ class EmployeeController extends AbstractController
             $employees => 'employees'
         ]);
     }
+
+    /**
+     * @Route("/ajouter", name="superAdmin_createEmployee", methods={"GET", "POST"})
+     */
+    public function createEmployee(Request $request,
+                             UserPasswordEncoderInterface $passwordEncoder,
+                             EntityManagerInterface $entityManager,
+                             UserRequestHandler $userRequestHandler)
+    {
+
+        $user = new UserRequest();
+
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()) {
+            if ($form->isValid()) {
+                $user = $userRequestHandler->registerAsAdmin($user);
+                $this->addFlash('success', 'Le compte de votre employé' . $user->getLastname() . ' ' . $user->getFirstname() . 'est créé');
+                return $this->redirectToRoute('login_route');
+            } else {
+                $this->addFlash('error', 'Le formulaire contient des erreurs');
+            }
+
+        }
+        return $this->render(
+            'security/register.html.twig',
+            [
+                'form' => $form->createView()
+            ]
+        );
+
+    }
+
 
 
 
