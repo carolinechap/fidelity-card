5be57ffb182803514edd5af7c3ea9eb20808aec4
Etudiant <resoclick@gmail.com>
2019-07-08 17:11:59 +0200

    Deal crud with translations and form WIP

--- src/Controller/DealController.php
+++ src/Controller/DealController.php
@@ -2,18 +2,95 @@
 
 namespace App\Controller;
 
+use App\Entity\Deal;
+use App\Form\DealType;
+use App\Repository\DealRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/deal")
+ */
 class DealController extends AbstractController
 {
     /**
-     * @Route("/deal", name="deal")
+     * @Route("/", name="deal_index", methods={"GET"})
      */
-    public function index()
+    public function index(DealRepository $dealRepository): Response
     {
         return $this->render('deal/index.html.twig', [
-            'controller_name' => 'DealController',
+            'deals' => $dealRepository->findAll(),
         ]);
+    }
+
+    /**
+     * @Route("/new", name="deal_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $deal = new Deal();
+        $form = $this->createForm(DealType::class, $deal);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($deal);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('deal_index');
+        }
+
+        return $this->render('deal/form.html.twig', [
+            'deal' => $deal,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="deal_show", methods={"GET"})
+     */
+    public function show(Deal $deal): Response
+    {
+        return $this->render('deal/show.html.twig', [
+            'deal' => $deal,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="deal_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Deal $deal): Response
+    {
+        $form = $this->createForm(DealType::class, $deal);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('deal_index', [
+                'id' => $deal->getId(),
+            ]);
+        }
+
+        return $this->render('deal/form.html.twig', [
+            'deal' => $deal,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="deal_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Deal $deal): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$deal->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($deal);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('deal_index');
     }
 }
--- src/Form/DealType.php
+++ src/Form/DealType.php
@@ -4,6 +4,10 @@ namespace App\Form;
 
 use App\Entity\Deal;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +16,34 @@ class DealType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('startDate')
-            ->add('endDate')
-            ->add('description')
-            ->add('costPoint')
+            ->add('name', TextType::class, [
+                'translation_domain' => 'forms',
+                'label' => 'deal.form.label.name'
+            ])
+            ->add('startDate',DateType::class, [
+                'label' => 'deal.form.label.startdate',
+                'required' => false,
+                'widget' => 'single_text',
+                'format' => 'dd/MM/yyyy',
+                'html5' => false,
+                'translation_domain' => 'forms'
+            ])
+            ->add('endDate',DateType::class, [
+                'label' => 'deal.form.label.enddate',
+                'required' => false,
+                'widget' => 'single_text',
+                'format' => 'dd/MM/yyyy',
+                'html5' => false,
+                'translation_domain' => 'forms'
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'deal.form.label.description',
+                'translation_domain' => 'forms'
+            ])
+            ->add('costPoint', NumberType::class, [
+                'label' => 'deal.form.label.costpoint',
+                'translation_domain' => 'forms'
+            ])
         ;
     }
 
