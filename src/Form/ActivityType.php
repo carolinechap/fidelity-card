4a0fc0c2fc3f084dd9ab0f32c54b302a1b18a7f8
CarolineChap <caroline2chapeau@gmail.com>
2019-07-09 10:17:34 +0200

    create/edit/delete functions, add form and template

--- src/Form/ActivityType.php
+++ src/Form/ActivityType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\Activity;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +15,29 @@ class ActivityType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('personalScore')
-            ->add('fidelityPoint')
-            ->add('gameDate')
-            ->add('isTheWinner')
+            ->add('personalScore', IntegerType::class,
+                [
+                    'label' => 'Points gagnés lors du jeu',
+
+                ])
+            //->add('fidelityPoint')
+            ->add('gameDate', DateType::class,
+                [
+                    'label' => 'Date du jeu',
+                    'required' => false,
+                    'widget' => 'single_text',
+                    'format' => 'dd/MM/yyyy',
+                    'html5' => false
+                ])
+            ->add('isTheWinner', ChoiceType::class,
+                [
+                    'label' => 'Match gagné',
+                    'choices' => [
+                        'Oui' => 1,
+                        'Non' => 0
+                    ]
+
+                ])
         ;
     }
 
