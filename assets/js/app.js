b6b72e53eb360c278d4d36a183d2590b27cc2431
Hugo LIEGEARD <hugo@hl-media.co>
2019-07-15 17:10:53 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,45 +16,57 @@ import 'bootstrap';
 //console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
 
 $(function() {
-    $.ajax({
-        paging: true,
-        pageLength: 5,
-        url : '/api/cards.json?order%5BpersonalScore%5D=desc&pagination=true', // La ressource ciblée
-        type : 'GET', // Le type de la requête HTTP
 
-        success : function(result) {
-            $.each(result, function (i, card) {
-                //console.log(result);
-                let classementDatas =
-                    '<tr><td scope="row">' + card.user.firstname + ' ' + card.user.lastname +  '</td> ' +
-                    '<td scope="row" class="text-center">' + card.personalScore + '</td>' +
-                    '<td scope="row" class="text-center">' + card.countVictory  + '/' + card.countGames + '</td>' +
-                    '<td scope="row" class="text-center">' + card.store.name + '</td></tr>';
-                $("#results").append(classementDatas);
-                //console.log(classementDatas);
-            })
-        },
-        error : function(result) {
-            let error =
-                '<th scope="row"> Oups... une erreur</th>';
-            $('#results').append(error);
-            console.log('erreur réseau');
-        }
+    // Init ranking process
+    getRankings();
+
+    // Called on pagination change
+    $('.pagination').on("click", ".page-link", function() {
+
+        $('#results').empty();
+        const page = $(this).attr('data-page');
+        getRankings(page);
+
     });
-    $('#demo').pagination({
-        dataSource: '/api/cards.json?order%5BpersonalScore%5D=desc&pagination=true',
-        locator: 'items',
-        totalNumber: 120,
-        pageSize: 20,
-        ajax: {
-            beforeSend: function() {
-                dataContainer.html('Loading data from flickr.com ...');
+
+    // Start ajax call
+    function getRankings(page = 1) {
+        $.ajax({
+            url : '/api/cards.jsonld?order%5BpersonalScore%5D=desc&pagination=true&page=' + page, // La ressource ciblée
+            type : 'GET', // Le type de la requête HTTP
+
+            success : function(result) {
+
+                const totalItems = result["hydra:totalItems"];
+                const itemsPerPage = 5;
+                const nbPages = (totalItems / itemsPerPage).toFixed(0);
+                $('.pagination').empty();
+
+                for (let i = 1; i <= nbPages; i++) {
+
+                    $(`<li class="page-item"><a class="page-link" data-page="${i}" href="javascript:void(0)">${i}</a></li>`).appendTo($('ul.pagination'));
+
+                }
+
+
+                $.each(result["hydra:member"], function (i, card) {
+                    //console.log(result);
+                    let classementDatas =
+                        '<tr><td scope="row">' + card.user.firstname + ' ' + card.user.lastname +  '</td> ' +
+                        '<td scope="row" class="text-center">' + card.personalScore + '</td>' +
+                        '<td scope="row" class="text-center">' + card.countVictory  + '/' + card.countGames + '</td>' +
+                        '<td scope="row" class="text-center">' + card.store.name + '</td></tr>';
+                    $("#results").append(classementDatas);
+                    //console.log(classementDatas);
+                })
+            },
+            error : function(result) {
+                let error =
+                    '<th scope="row"> Oups... une erreur</th>';
+                $('#results').append(error);
+                console.log('erreur réseau');
             }
-        },
-        callback: function(data, pagination) {
-            // template method of yourself
-            var html = template(data);
-            dataContainer.html(html);
-        }
-    })
+        });
+    }
+    
 });
