a230bbf38163f63c522f0e34fecd78c8fd522b8c
CarolineChap <caroline2chapeau@gmail.com>
2019-07-22 15:01:45 +0200

    :zap: validation users/employees adress

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -13,6 +13,8 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\NotNull;
 
 /**
  * Class UserType
@@ -33,27 +35,21 @@ class UserType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $required = true;
-
-        if($this->security->isGranted(['ROLE_SUPERADMIN'])){
-
-            $required = false;
-        }
 
         $builder
             ->add('lastname',
-            TextType::class,
-            [
-                'translation_domain' => 'forms',
-                'label' => 'user.form.label.lastname',
-            ]
+                TextType::class,
+                [
+                    'translation_domain' => 'forms',
+                    'label' => 'user.form.label.lastname',
+                ]
             )
             ->add('firstname',
                 TextType::class,
                 [
                     'translation_domain' => 'forms',
                     'label' => 'user.form.label.firstname',
-                ]            )
+                ])
             ->add('email',
                 EmailType::class,
                 [
@@ -73,55 +69,116 @@ class UserType extends AbstractType
                         'label' => 'user.form.label.confirmation_password',
                     ],
                     'invalid_message' => 'La confirmation doit correspondre au mot de passe'
-                    ]
-            )
-            ->add('numberStreet',
-                TextType::class,
-                [
-                    'translation_domain' => 'forms',
-                    'label' => 'user.form.label.number_street',
-                    'required' => $required
+                ]
+            );
+
+        if (!$this->security->isGranted(['ROLE_SUPERADMIN'])) {
+            $builder
+                ->add('numberStreet',
+                    TextType::class,
+                    [
+                        'translation_domain' => 'forms',
+                        'label' => 'user.form.label.number_street',
+                        'required' => true,
+                        'constraints' => [
+                            new NotBlank(),
+                        ]
                     ]
                 )
-            ->add('nameStreet',
-                TextType::class,
-                [
-                    'translation_domain' => 'forms',
-                    'label' => 'user.form.label.name_street',
-                    'required' => $required
-                ]
-            )
-            ->add('zipCode',
-                TextType::class,
-                [
-                    'translation_domain' => 'forms',
-                    'label' => 'user.form.label.zip_code',
-                    'required' => $required
-                ]
-            )
-            ->add('city',
-                TextType::class,
-                [
-                    'translation_domain' => 'forms',
-                    'label' => 'user.form.label.city',
-                    'required' => $required
-                ]
-            )
-            ->add('country',
-                TextType::class,
-                [
-                    'translation_domain' => 'forms',
-                    'label' => 'user.form.label.country',
-                    'required' => $required
-                ]
-            )
-        ;
+                ->add('nameStreet',
+                    TextType::class,
+                    [
+                        'translation_domain' => 'forms',
+                        'label' => 'user.form.label.name_street',
+                        'required' => true,
+                        'constraints' => [
+                            new NotBlank(),
+                        ]
+                    ]
+                )
+                ->add('zipCode',
+                    TextType::class,
+                    [
+                        'translation_domain' => 'forms',
+                        'label' => 'user.form.label.zip_code',
+                        'required' => true,
+                        'constraints' => [
+                            new NotBlank(),
+                        ]
+                    ]
+                )
+                ->add('city',
+                    TextType::class,
+                    [
+                        'translation_domain' => 'forms',
+                        'label' => 'user.form.label.city',
+                        'required' => true,
+                        'constraints' => [
+                            new NotBlank(),
+                        ]
+                    ]
+                )
+                ->add('country',
+                    TextType::class,
+                    [
+                        'translation_domain' => 'forms',
+                        'label' => 'user.form.label.country',
+                        'required' => true,
+                        'constraints' => [
+                            new NotBlank(),
+                        ]
+                    ]
+                );
+        } else {
+            $builder
+                ->add('numberStreet',
+                    TextType::class,
+                    [
+                        'translation_domain' => 'forms',
+                        'label' => 'user.form.label.number_street',
+                        'required' => false,
+                    ]
+                )
+                ->add('nameStreet',
+                    TextType::class,
+                    [
+                        'translation_domain' => 'forms',
+                        'label' => 'user.form.label.name_street',
+                        'required' => false,
+                    ]
+                )
+                ->add('zipCode',
+                    TextType::class,
+                    [
+                        'translation_domain' => 'forms',
+                        'label' => 'user.form.label.zip_code',
+                        'required' => false,
+                    ]
+                )
+                ->add('city',
+                    TextType::class,
+                    [
+                        'translation_domain' => 'forms',
+                        'label' => 'user.form.label.city',
+                        'required' => false,
+                    ]
+                )
+                ->add('country',
+                    TextType::class,
+                    [
+                        'translation_domain' => 'forms',
+                        'label' => 'user.form.label.country',
+                        'required' => false,
+                    ]
+                );
+        }
     }
 
     /**
      * @param OptionsResolver $resolver
      */
-    public function configureOptions(OptionsResolver $resolver)
+    public
+    function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
             'data_class' => UserRequest::class,
